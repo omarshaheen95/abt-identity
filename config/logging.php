580fc9467d646bf6ecd9c08984743b8083aa932f
Osama <Osama20019>
2024-12-04 13:27:28 +0200

    Add Telegram log

--- config/logging.php
+++ config/logging.php
@@ -117,6 +117,11 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+        'telegram' => [
+            'driver' => 'monolog',
+            'handler' => \App\Exceptions\TelegramLoggerHandler::class,
+            'level' => 'debug',
+        ],
     ],
 
 ];

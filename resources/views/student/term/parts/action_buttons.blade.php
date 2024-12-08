7b1956440b39fc839bf079ee6141556596ed021b
Osama <Osama20019>
2024-12-08 13:31:08 +0200

    Update Local #2

--- resources/views/student/term/parts/action_buttons.blade.php
+++ resources/views/student/term/parts/action_buttons.blade.php
@@ -4,11 +4,11 @@
     @endif
 
     @if(isset($tab_index)&& $tab_index!=3)
-        <a class="btn btn-next" data-tab-index="{{$tab_index}}">{{t('Next'[],$term->level->arab?'ar':'en')}}</a>
+        <a class="btn btn-next" data-tab-index="{{$tab_index}}">{{t('Next',[],$term->level->arab?'ar':'en')}}</a>
     @else
         <div class="btn btn-submit">
             <span class="spinner-border spinner-border-sm me-2 d-none"></span>
-            {{t('Finish'[],$term->level->arab?'ar':'en')}}
+            {{t('Finish',[],$term->level->arab?'ar':'en')}}
         </div>
     @endif
 

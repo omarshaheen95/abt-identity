148b31b4870b449ef6172d83d231efb644b128c5
Osama <Osama20019>
2024-12-10 13:34:36 +0200

    Update Multiple Choice Preview

--- resources/views/manager/student_term/term_preview/questions/choose.blade.php
+++ resources/views/manager/student_term/term_preview/questions/choose.blade.php
@@ -27,24 +27,12 @@
             <div class="answer-group d-flex justify-content-center align-items-start py-2">
                 @foreach($question->option_question as $option)
                     <div class="form-check form-check-inline align-items-center">
-                        @if($term->level->arab)
-                            @if(!$option->image)
-                                <label class="form-check-label ms-2" for="choose-option-{{$option->id}}">
-                                    {{$option->content}}
-                                </label>
-                            @else
-                                <div class="option-pic ms-2">
-                                    <img src="{{asset($option->image)}}"/>
-                                </div>
-                            @endif
-                        @endif
 
                         <input class="form-check-input" type="radio"
                                name="questions[{{$question->id}}][answer_option_id]"
                                id="choose-option-{{$option->id}}" value="{{$option->id}}"
-                               @if($question->result==$option->id)checked @endif>
+                               @if($option->result == 1)checked @endif>
 
-                        @if(!$term->level->arab)
                             @if(!$option->image)
                                 <label class="form-check-label ms-2" for="choose-option-{{$option->id}}">
                                     {{$option->content}}
@@ -54,7 +42,6 @@
                                     <img src="{{asset($option->image)}}"/>
                                 </div>
                             @endif
-                        @endif
                     </div>
                 @endforeach
 

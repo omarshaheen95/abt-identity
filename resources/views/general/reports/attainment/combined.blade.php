ec8fc1b10deb25ff8b0432f35041e8c6ef055567
omarshaheen95 <34618796+omarshaheen95@users.noreply.github.com>
2025-01-19 14:29:38 +0200

    update report

--- resources/views/general/reports/attainment/combined.blade.php
+++ resources/views/general/reports/attainment/combined.blade.php
@@ -359,7 +359,7 @@
                                     <table class="table small m-0">
                                         <thead>
                                         <tr>
-                                            <th class="main-th"> {{re('Assessment')}} ({{$skill->name}})</th>
+                                            <th class="main-th"> {{re('Assessment')}} ({{re($skill->name)}})</th>
                                             <th class="below-td"> {{re('Below')}}</th>
                                             <th class="inline-td"> {{re('Inline')}}</th>
                                             <th class="above-td"> {{re('Above')}}</th>
@@ -786,7 +786,7 @@
                                     <table class="table small m-0">
                                         <thead>
                                         <tr>
-                                            <th class="main-th"> {{re('Assessment')}} ({{$skill->name}})</th>
+                                            <th class="main-th"> {{re('Assessment')}} ({{re($skill->name)}})</th>
                                             <th class="below-td"> {{re('Below')}}</th>
                                             <th class="inline-td"> {{re('Inline')}}</th>
                                             <th class="above-td"> {{re('Above')}}</th>

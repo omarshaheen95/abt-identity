ba1b34976c85a7e913c90af8db5591ede4ee9ea6
Osama <Osama20019>
2025-01-23 15:32:37 +0200

    Show Grade

--- resources/views/manager/student_term/index.blade.php
+++ resources/views/manager/student_term/index.blade.php
@@ -140,7 +140,6 @@
              <th class="text-start">{{t('Name')}}</th>
              <th class="text-start">{{t('Email')}}</th>
              <th class="text-start">{{t('School')}}</th>
-             <th class="text-start">{{t('Grade')}}</th>
              <th class="text-start">{{t('Year')}}</th>
              <th class="text-start">{{t('Round')}}</th>
             @if(request('status')=='corrected')
@@ -165,7 +164,6 @@
             {data: 'name', name: 'name'},
             {data: 'email', name: 'email'},
             {data: 'school', name: 'school'},
-            {data: 'grade_name', name: 'grade_name'},
             {data: 'year', name: 'year'},
             {data: 'round', name: 'round'},
             @if(request('status')=='corrected')

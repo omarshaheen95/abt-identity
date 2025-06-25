d345598c5ef0688cd388594fd9862b8d475ff8fa
OsamaAl Tawil <osamafffayez2000@gmail.com>
2025-06-25 12:10:42 +0300

    Update Table Style And Filter Place

--- resources/views/general/layout_parts/style.blade.php
+++ resources/views/general/layout_parts/style.blade.php
@@ -17,7 +17,7 @@
     <link href="{{asset('assets_v1/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css"/>
     <link href="{{asset('assets_v1/css/style.bundle.css')}}?v={{time()}}" rel="stylesheet" type="text/css"/>
 @endif
-<link href="{{asset('assets_v1/css/table-style.css')}}?v={{time()}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('assets_v1/css/table-style.css')}}?v=3" rel="stylesheet" type="text/css"/>
 
 <style>
     /*style to disable item menu sidebar */
